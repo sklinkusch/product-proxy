88916a46f80f67fd8d354b6ba496e96b7d564457
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-04 14:09:10 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+require('isomorphic-fetch');
+require('now-env');
+
+const { parse } = require('url');
+const { send } = require('micro');
+
+module.exports = async (req, res) => {
+  const { query } = parse(req.url);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  const requestUrl = `http://world.openfoodfacts.org/api/v0/product/${query}.json`;
+  fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => {
+      const {
+        code,
+        product: { code: pcode, brands, categories, product_name, quantity },
+        status,
+        status_verbose,
+      } = data;
+      const newData = {
+        code,
+        product: { code: pcode, brands, categories, product_name, quantity },
+        status,
+        status_verbose,
+      };
+      send(res, 200, newData);
+    })
+    .catch(error => {
+      send(res, 500, error);
+    });
+};
